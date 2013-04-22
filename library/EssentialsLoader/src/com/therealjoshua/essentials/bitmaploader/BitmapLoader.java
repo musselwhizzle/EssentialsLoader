dcf54fc935d586887d5b47bbc9017c6b6f9bb253
Musselwhizzle <jm@therealjoshua.com>
2013-04-22 10:50:10 -0700

    added copyright information

--- library/EssentialsLoader/src/com/therealjoshua/essentials/bitmaploader/BitmapLoader.java
+++ library/EssentialsLoader/src/com/therealjoshua/essentials/bitmaploader/BitmapLoader.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
- *
+ * Copyrigth (c) 2012 Joshua Musselwhite
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
