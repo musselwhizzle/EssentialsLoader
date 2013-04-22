6b8e4c3a03b9b960c3daacf8f8f081561cab30c2
Musselwhizzle <jm@therealjoshua.com>
2013-04-22 12:52:42 -0700

    fixed spelling in docs

--- library/EssentialsLoader/src/com/therealjoshua/essentials/bitmaploader/cache/LruCacheAdapter.java
+++ library/EssentialsLoader/src/com/therealjoshua/essentials/bitmaploader/cache/LruCacheAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyrigth (c) 2012 Joshua Musselwhite
+ * Copyright (c) 2012 Joshua Musselwhite
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
